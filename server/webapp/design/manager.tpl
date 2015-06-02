6838068682abb8e9af50fc1ec504809fe3db9f68
Marvin Therolf <marvin.therolf@gmail.com>
2015-06-02 11:43:03 +0200

    started work on manager screen

--- server/webapp/design/manager.tpl
+++ server/webapp/design/manager.tpl
@@ -1,10 +1,1 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-4">
-            <panel></panel>
-        </div>
-        <div class="col-md-4">
-            <panel></panel>
-        </div>
-   </div>
-</div>
+<managerContainer></managerContainer>
---
+++ server/webapp/js/managerContainer/managerContainer.js
@@ -0,0 +1,22 @@
+/**
+ * Created by captainluma on 02.06.15.
+ */
+
+var managerContainer = Ractive.extend(
+    {
+        template : '\
+        <div class="manager-container">\
+            <div class="container">\
+                <div class="row">\
+                    <div class="col-sm-4">\
+                        <div class="panel panel-primary info-panel">\
+                            <div class="panel-heading">Hi Chris</div>\
+                            <div class="panel-body">Bye Chris</div>\
+                        </div>\
+                   </div>\
+                </div>\
+            </div>\
+        </div>\
+        '
+    }
+);
