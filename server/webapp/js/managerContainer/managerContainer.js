f63fb7a0e4694b8b9390fbc2a91b1916a39a74c6
Marvin Therolf <marvin.therolf@gmail.com>
2015-06-02 11:58:24 +0200

    added a second panel to manager screen

--- server/webapp/js/managerContainer/managerContainer.js
+++ server/webapp/js/managerContainer/managerContainer.js
@@ -8,6 +8,17 @@ var managerContainer = Ractive.extend(
         <div class="manager-container">\
             <div class="container">\
                 <div class="row">\
+                    <div class="container"><p>current/container/path</p></div>\
+                </div>\
+                \
+                \
+                <div class="row">\
+                    <div class="col-sm-4">\
+                        <div class="panel panel-primary info-panel">\
+                            <div class="panel-heading">Hi Chris</div>\
+                            <div class="panel-body">Bye Chris</div>\
+                        </div>\
+                    </div>\
                     <div class="col-sm-4">\
                         <div class="panel panel-primary info-panel">\
                             <div class="panel-heading">Hi Chris</div>\
