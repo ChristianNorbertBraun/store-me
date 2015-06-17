2f0874243af9d8a470ea80baa992a61bac5c56a2
Marvin Therolf <marvin.therolf@gmail.com>
2015-06-17 11:21:46 +0200

    relocated data_structure and dbConnection directories to js directory. created stock and deplete functions. wrote docu. deleted jsdoc output directory.

---
+++ server/webapp/js/storageFunctions/storage_functions.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Marvin Therolf and Marcel Groß on 17.06.15.
+ */
+
+/**
+ * Stocks an amount of items into a given storage.
+ * @function
+ * @param {Container} storage   - Root Container object of used storage
+ * @param {String} containerID  - Unique Identifier of container to stock item(s) into
+ * @param {String} itemID       - Unique Identifier of item(s) to stock into container
+ * @param {Number} amount       - Amount of items to stock
+ * @author Marvin Therolf and Marcel Groß
+ */
+var stock = function(storage, containerID, itemID, amount)
+{
+    var container = getContainerById(storage, containerID);
+    addItem(container, itemID, amount);
+};
+
+/**
+ * Depletes an amount of items from a given storage.
+ * @function
+ * @param {Container} storage   - Root Container object of used storage
+ * @param {String} containerID  - Unique Identifier of container to deplete item(s) from
+ * @param {String} itemID       - Unique Identifier of item(s) to deplete from container
+ * @param {Number} amount       - Amount of items to deplete
+ * @author Marvin Therolf and Marcel Groß
+ */
+var deplete = function(storage, containerID, itemID, amount)
+{
+    var container = getContainerById(storage, containerID);
+    removeItem(container, itemID, amount);
+};
