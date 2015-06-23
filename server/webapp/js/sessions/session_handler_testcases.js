2f215171397e2f48a662ceec977ce71ee9168e8a
Marvin Therolf <marvin.therolf@gmail.com>
2015-06-23 20:35:33 +0200

    session ids are working properly

--- server/webapp/js/sessions/session_handler_testcases.js
+++ server/webapp/js/sessions/session_handler_testcases.js
@@ -4,6 +4,12 @@
 
 var session = new Session("captainLuma", "cherrybanana");
 console.log(currentSessions.length === 1);
-setTimeout("console.log(isValidSession(session.sessionID) === false)", 700000); // test with 1 min expire time and 70s of waiting worked
-console.log("hi");
-getSessionIDFromURL();
+//setTimeout("console.log(isValidSession(session.sessionID) === false)", 700000); // test with 1 min expire time and 70s of waiting worked
+
+var timeStamp = 1435083694726;
+console.log(digitSum(timeStamp));
+console.log(getCheckStamp("marvintherolf"));
+console.log(getHashCode("marvintherolfcherrybanana", timeStamp));
+console.log(getSessionID("marvintherolf", "cherrybanana", timeStamp));
+
+// TODO: test multiple sessions
