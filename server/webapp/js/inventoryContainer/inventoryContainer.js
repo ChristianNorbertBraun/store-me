6476a087cb5dcea52dba16004a446940daae516d
Marcel <marcel.gross@4ebf.de>
2015-06-30 21:37:13 +0200

    test

--- server/webapp/js/inventoryContainer/inventoryContainer.js
+++ server/webapp/js/inventoryContainer/inventoryContainer.js
@@ -9,7 +9,7 @@ var inventoryContainer = Ractive.extend({
                 <div class="col-sm-3">\
                 \
                     <div id="filter" class="input-group">\
-                        <input on-change="filterItems()" value={{filter}} type="text" class="form-control" placeholder="Filter...">\
+                        <input on-change="filterItemsByAttributes()" value={{filter}} type="text" class="form-control" placeholder="Filter...">\
                         <span class="input-group-addon glyphicon glyphicon-search"></span>\
                     </div>\
                     \
@@ -19,9 +19,17 @@ var inventoryContainer = Ractive.extend({
                         </div>\
                         \
                         <div class="panel-body">\
-                            \
-                            {{#each attributes}}\
-                                <p>{{attributeName}}</p>\
+                            <div class="checkbox attributes-entry">\
+                                <label>\
+                                    <input on-change="filterItemsByAttributes()" checked={{allAttributes}} type="checkbox"><p class="checkbox-label">  Show All Attributes</p>\
+                                </label>\
+                            </div>\
+                            {{#each attributes:i}}\
+                                <div class="checkbox attributes-entry">\
+                                    <label>\
+                                        <input on-click="filterItemsByAttributes()" checked={{.status}} type="checkbox"><p class="checkbox-label">  {{attributeName}}</p>\
+                                    </label>\
+                                </div>\
                             {{/each}}\
                         </div>\
                     </div>\
@@ -43,7 +51,7 @@ var inventoryContainer = Ractive.extend({
                             <tbody>\
                                 {{#each items:i}}\
                                     <tr id="item_{{i}}">\
-                                        <td>{{itemID}}</td>\
+                                        <td>{{_id}}</td>\
                                         <td>{{name}}</td>\
                                         <td>{{category_id}}</td>\
                                         <td>{{parentContainerName}}</td>\
@@ -62,13 +70,15 @@ var inventoryContainer = Ractive.extend({
         items: null,         // all items for item table
         filteredItems: null,
         backupItems: null,
-        attributes: null     // all attributes for attributes panel
+        attributes: null,    // all attributes for attributes panel
+        allAttributes: true,
+        filteredItemsByAttribute: null
     },
 
     oninit: function() {
         window.currentRactive = this;           // often can't access ractive so we save it using a global var
         window.currentRactive.refreshItems();
-        window.currentRactive.refreshAttributes();
+        //window.currentRactive.refreshAttributes();
     },
 
     oncomplete: function() {
@@ -77,7 +87,7 @@ var inventoryContainer = Ractive.extend({
 
     /* items */
 
-    refreshItems: function() {
+    /*refreshItems: function() {
         loadStore(function(success, container) {
             if (success) {
                 var items = getAllItems(container);
@@ -96,10 +106,39 @@ var inventoryContainer = Ractive.extend({
                 });
             }
         });
+    },*/
+
+    refreshItems: function() {
+        loadStore(function(success, container) {
+            window.storage = container;
+            var allContainerItems = getAllItems(window.storage);
+            window.containerItems = allContainerItems;
+
+            getDataItems(allContainerItems,function(success, data){
+                if(success){
+                    for(i = 0; i < window.containerItems.length; ++i){
+
+                        data[i].amount = window.containerItems[i].amount;
+                        data[i].parentContainerID = window.containerItems[i].parentContainerID;
+                        var containerName = getContainerById(window.storage,data[i].parentContainerID).containerName;
+                        data[i].parentContainerName = containerName;
+                    }
+
+                    window.currentRactive.set('items',data);
+                    window.currentRactive.set('backupItems', data);
+                }
+                else{
+                    window.currentRactive.set('items',data);
+                    window.currentRactive.set('backupItems', data);
+                }
+
+                window.currentRactive.refreshAttributes();
+            });
+        });
     },
 
     /* attributes */
-
+    /*
     refreshAttributes: function() {
         loadStore(function(success, container) {
             if (success) {
@@ -118,11 +157,45 @@ var inventoryContainer = Ractive.extend({
                             addAttributes(attributes, data);
                         }
 
+                        /* since we don't know the index just set everytime the whole status on false
+                        attributes.map(function(attr) {
+                            attr.status = true;
+                        });
+
                         window.currentRactive.set('attributes', attributes);
                     });
                 })
             }
         });
+    },*/
+
+    refreshAttributes: function() {
+        /* load all items */
+        var allItems = window.currentRactive.get('items');
+        var allAttributes = [];
+
+        /* iterate over every item */
+        allItems.map(function (item) {
+            if (item.attributes != null) {
+
+                /* nested for loops to check whether an attribute is already in the attributes array */
+                for (var i = 0; i < item.attributes.length; i++) {
+                    var newAttribute = true;
+
+                    for (var j = 0; j < allAttributes.length; j++) {
+                        if (item.attributes[i].attributeName == allAttributes[j].attributeName) {
+                            newAttribute = false;
+                        }
+                    }
+
+                    if (newAttribute) {
+                        allAttributes.push(item.attributes[i]);
+                    }
+                }
+            }
+        });
+
+        window.currentRactive.set('attributes', allAttributes);
     },
 
     /* filter */
@@ -154,7 +227,7 @@ var inventoryContainer = Ractive.extend({
     isFiltered: function(item, filter) {
         if (
             (item.name.indexOf(filter) == -1) &&
-            (item.itemID.indexOf(filter) == -1) &&
+            (item._id.indexOf(filter) == -1) &&
             (item.category_id.indexOf(filter) == -1) &&
             (item.parentContainerName.indexOf(filter) == -1)
         ) {
@@ -162,6 +235,51 @@ var inventoryContainer = Ractive.extend({
         }
         else {
             return false;
+        }
+    },
+
+    filterItemsByAttributes: function() {
+        var allAttributesGetShown = window.currentRactive.get('allAttributes');
+
+        if (!allAttributesGetShown) {
+            console.log("filter");
+            window.currentRactive.filterItems();
+
+            var currentItems = window.currentRactive.get('items');
+            var filterArray = currentItems.slice();
+            var filteredAttributes = window.currentRactive.get('attributes');
+
+            /*filteredAttributes.map(function (attr) {
+                if (attr.status == false) {
+                    for (var i = 0; i < filterArray.length; i++) {
+                        console.log(filterArray[i]);
+                        window.currentRactive.checkAndRemoveAttributes(filterArray[i], attr.attributeName);
+                    }
+                }
+            });*/
+
+            for (var i = 0; i < filterArray.length; i++) {
+                if (filterArray[i].attributes == null || filterArray[i].attributes.length == 0) {
+                    filterArray.splice(i, 1);
+                    i--;
+                }
+            }
+
+            window.currentRactive.set('items', filterArray);
+        }
+        else {
+            window.currentRactive.filterItems();
+            console.log("show all");
+        }
+    },
+
+    checkAndRemoveAttributes: function(item, attrName) {
+        if (item.attributes != null) {
+            for (var i = 0; i < item.attributes.length; i++) {
+                if (item.attributes[i] == attrName) {
+                    item.attributes.splice(i, 1);
+                }
+            }
         }
     }
 });
